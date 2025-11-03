801143eba3e079bb904830fef25e5402bd8baa17
Demo436 <kushalgowda@gmail.com>
2025-11-03 19:44:17 +0530

    Added backend url

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173", // React dev server
+  origin: "https://clustorix-admin.onrender.com", // React dev server
   credentials: true,
 }));
 
