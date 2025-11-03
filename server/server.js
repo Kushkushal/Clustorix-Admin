1a93b46d8cdc99231e6aeee998898cba0fd3cc43
Demo436 <kushalgowda@gmail.com>
2025-11-03 19:50:06 +0530

    Addedd

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://clustorix-admin.onrender.com", // React dev server
+  origin: "https://clustorix-admin-frontend.onrender.com", // React dev server
   credentials: true,
 }));
 
